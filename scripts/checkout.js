accc1c695eacf55814508d25e7341f9c253aee25
Mansur <ristale86@gmail.com>
2025-10-01 22:22:09 +0300

    cleanup comments

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,8 +2,6 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts, loadProductsFetch} from '../data/products.js';
 import {loadCartFetch} from '../data/cart.js';
-// import '../data/cart-class.js';
-// import '../data/backend-practice.js';
 
 async function loadPage() {
   try {
@@ -19,49 +17,3 @@ async function loadPage() {
   renderPaymentSummary();
 }
 loadPage();
-
-/*
-Promise.all([
-  loadProductsFetch(),
-  new Promise((resolve) => {
-    loadCart(() => {
-      resolve();
-    });
-  })
-
-]).then((values) => {
-  console.log(values);
-  renderOrderSummary();
-  renderPaymentSummary();
-});
-*/
-
-/*
-new Promise((resolve) => {
-  loadProducts(() => {
-    resolve('value1');
-  });
-
-}).then((value) => {
-  console.log(value);
-
-  return new Promise((resolve) => {
-    loadCart(() => {
-      resolve();
-    });
-  });
-
-}).then(() => {
-  renderOrderSummary();
-  renderPaymentSummary();
-});
-*/
-
-/*
-loadProducts(() => {
-  loadCart(() => {
-    renderOrderSummary();
-    renderPaymentSummary();
-  });
-});
-*/
