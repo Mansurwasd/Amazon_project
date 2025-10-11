3ef4e0571c19e92b3d0c9c9a3cf8f21db5545084
Mansur <ristale86@gmail.com>
2025-10-11 11:40:56 +0300

    added api, requirements

--- backend/restfull_api.py
+++ backend/restfull_api.py
@@ -1,6 +1,150 @@
-[
+"""для генерации случайных id"""
+import uuid
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import uvicorn
+app = FastAPI()
+
+orders = []
+origins = [
+    "http://127.0.0.1:5500"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class CartItem(BaseModel):
+    """класс предметов в корзине"""
+    productId: str
+    deliveryDate: str
+    quantity: int = 0
+
+
+class Order(BaseModel):
+    """класс заказов пользователя"""
+    userId: str = None
+    orderId: str = uuid.uuid4().hex
+    totalPriceCents: int = None
+    placingDate: str = None
+    products: list[CartItem] = []
+
+class Rating(BaseModel):
+    """класс рейтинга предмета"""
+    stars: float = None
+    count: int = None
+class Item(BaseModel):
+    """класс предметов заказа"""
+    itemId: str = uuid.uuid4().hex
+    image: str = None
+    name: str = None
+    rating: Rating
+    priceCents: int = None
+    type: str = None
+    keywords: list[str] = []
+    sizeChartLink: str = None
+
+@app.get("/orders")
+async def get_orders():
+    """возвращает список всех заказов"""
+    return orders[::-1]
+
+@app.get("/items")
+async def get_items():
+    """возвращает список всех предметов"""  #пользователь
+    return items
+
+
+@app.get("/orders/{order_id}")
+async def get_order(order_id: str):
+    """возвращает заказ по id"""
+    for i in orders:
+        if i["orderId"] == order_id:
+            return i
+    raise HTTPException(status_code=404, detail=f"Order {order_id} not found")
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: str):
+    """возвращает предмет по id"""
+    for i in items:
+        if i["itemId"] == item_id:
+            return i
+    raise HTTPException(status_code=404, detail=f"Item {item_id} not found") #пользователь
+
+@app.post("/items")
+async def create_item(item: list[Item]):
+    """создает предмет"""
+    for i in item:
+        items.append(i)
+    return item
+
+@app.post("/orders")
+async def create_order(order: Order):
+    """создает заказ"""
+    orders.append(order) #пользователь
+    return orders
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: str, item: Item):
+    """изменяет поля предмета по id"""
+    for i, preitem in enumerate(items):
+        if preitem["itemId"] == item_id:
+            items[i] = item
+            return {"status": "200"}
+    raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+
+@app.put("/orders/{order_id}")
+async def update_order(order_id: str, order: Order):
+    """изменяет поля заказа по id"""
+    for i, preorder in enumerate(orders):
+        if preorder["orderId"] == order_id:
+            orders[i] = order
+            return {"status": "200"}
+    raise HTTPException(status_code=404, detail=f"Order {order_id} not found")
+
+@app.delete("/orders/{order_id}")
+async def delete_order(order_id: str):
+    """удаляет заказ по id"""
+    for i, order in enumerate(orders):
+        if order["orderId"] == order_id:
+            orders.pop(i)
+            return {"status": "200"}
+    raise HTTPException(status_code=404, detail=f"Order {order_id} not found")
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: str):
+    """удаляет предмет по id"""
+    for i, item in enumerate(items):
+        if item["itemId"] == item_id:
+            items.pop(i)
+            return {"status": "200"}
+    raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+
+
+@app.get("/users/{user_id}")
+async def get_users_orders(user_id: str):
+    """возвращает заказы по id пользователя которому принадлежит эти заказы"""
+    orders_of_user = []
+    for i in orders:
+        if i.userId == user_id:
+            orders_of_user.append(i)  #пользователь
+    return orders_of_user
+
+def start_server():
+    """запускает сервер"""
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+if __name__ == "__main__":
+    start_server()
+
+items = [
   {
-    "id": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    "itemId": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     "image": "images/products/athletic-cotton-socks-6-pairs.jpg",
     "name": "Black and Gray Athletic Cotton Socks - 6 Pairs",
     "rating": {
@@ -11,7 +155,7 @@
     "keywords": ["socks", "sports", "apparel"]
   },
   {
-    "id": "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    "itemId": "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     "image": "images/products/intermediate-composite-basketball.jpg",
     "name": "Intermediate Size Basketball",
     "rating": {
@@ -22,7 +166,7 @@
     "keywords": ["sports", "basketballs"]
   },
   {
-    "id": "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    "itemId": "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
     "image": "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
     "name": "Adults Plain Cotton T-Shirt - 2 Pack",
     "rating": {
@@ -35,7 +179,7 @@
     "sizeChartLink": "images/clothing-size-chart.png"
   },
   {
-    "id": "54e0eccd-8f36-462b-b68a-8182611d9add",
+    "itemId": "54e0eccd-8f36-462b-b68a-8182611d9add",
     "image": "images/products/black-2-slot-toaster.jpg",
     "name": "2 Slot Toaster - Black",
     "rating": {
@@ -46,7 +190,7 @@
     "keywords": ["toaster", "kitchen", "appliances"]
   },
   {
-    "id": "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    "itemId": "3ebe75dc-64d2-4137-8860-1f5a963e534b",
     "image": "images/products/6-piece-white-dinner-plate-set.jpg",
     "name": "6 Piece White Dinner Plate Set",
     "rating": {
@@ -57,7 +201,7 @@
     "keywords": ["plates", "kitchen", "dining"]
   },
   {
-    "id": "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    "itemId": "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
     "image": "images/products/6-piece-non-stick-baking-set.webp",
     "name": "6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set",
     "rating": {
@@ -68,7 +212,7 @@
     "keywords": ["kitchen", "cookware"]
   },
   {
-    "id": "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    "itemId": "dd82ca78-a18b-4e2a-9250-31e67412f98d",
     "image": "images/products/plain-hooded-fleece-sweatshirt-yellow.jpg",
     "name": "Plain Hooded Fleece Sweatshirt",
     "rating": {
@@ -79,7 +223,7 @@
     "keywords": ["hoodies", "sweaters", "apparel"]
   },
   {
-    "id": "77919bbe-0e56-475b-adde-4f24dfed3a04",
+    "itemId": "77919bbe-0e56-475b-adde-4f24dfed3a04",
     "image": "images/products/luxury-tower-set-6-piece.jpg",
     "name": "Luxury Towel Set - Graphite Gray",
     "rating": {
@@ -90,7 +234,7 @@
     "keywords": ["bathroom", "washroom", "restroom", "towels", "bath towels"]
   },
   {
-    "id": "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
+    "itemId": "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
     "image": "images/products/liquid-laundry-detergent-plain.jpg",
     "name": "Liquid Laundry Detergent, 110 Loads, 82.5 Fl Oz",
     "rating": {
@@ -101,7 +245,7 @@
     "keywords": ["bathroom", "cleaning"]
   },
   {
-    "id": "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+    "itemId": "58b4fc92-e98c-42aa-8c55-b6b79996769a",
     "image": "images/products/knit-athletic-sneakers-gray.jpg",
     "name": "Waterproof Knit Athletic Sneakers - Gray",
     "rating": {
@@ -112,7 +256,7 @@
     "keywords": ["shoes", "running shoes", "footwear"]
   },
   {
-    "id": "5968897c-4d27-4872-89f6-5bcb052746d7",
+    "itemId": "5968897c-4d27-4872-89f6-5bcb052746d7",
     "image": "images/products/women-chiffon-beachwear-coverup-black.jpg",
     "name": "Women's Chiffon Beachwear Cover Up - Black",
     "rating": {
@@ -125,7 +269,7 @@
     "sizeChartLink": "images/clothing-size-chart.png"
   },
   {
-    "id": "aad29d11-ea98-41ee-9285-b916638cac4a",
+    "itemId": "aad29d11-ea98-41ee-9285-b916638cac4a",
     "image": "images/products/round-sunglasses-black.jpg",
     "name": "Round Sunglasses",
     "rating": {
@@ -136,7 +280,7 @@
     "keywords": ["accessories", "shades"]
   },
   {
-    "id": "04701903-bc79-49c6-bc11-1af7e3651358",
+    "itemId": "04701903-bc79-49c6-bc11-1af7e3651358",
     "image": "images/products/women-beach-sandals.jpg",
     "name": "Women's Two Strap Buckle Sandals - Tan",
     "rating": {
@@ -147,7 +291,7 @@
     "keywords": ["footwear", "sandals", "womens", "beach", "summer"]
   },
   {
-    "id": "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
+    "itemId": "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
     "image": "images/products/blackout-curtain-set-beige.webp",
     "name": "Blackout Curtains Set 4-Pack - Beige",
     "rating": {
@@ -158,7 +302,7 @@
     "keywords": ["bedroom", "curtains", "home"]
   },
   {
-    "id": "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
+    "itemId": "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
     "image": "images/products/men-slim-fit-summer-shorts-gray.jpg",
     "name": "Men's Slim-Fit Summer Shorts",
     "rating": {
@@ -169,7 +313,7 @@
     "keywords": ["shorts", "apparel", "mens"]
   },
   {
-    "id": "c2a82c5e-aff4-435f-9975-517cfaba2ece",
+    "itemId": "c2a82c5e-aff4-435f-9975-517cfaba2ece",
     "image": "images/products/electric-glass-and-steel-hot-water-kettle.webp",
     "name": "Electric Glass and Steel Hot Tea Water Kettle - 1.7-Liter",
     "rating": {
@@ -180,7 +324,7 @@
     "keywords": ["water boiler", "appliances", "kitchen"]
   },
   {
-    "id": "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
+    "itemId": "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
     "image": "images/products/facial-tissue-2-ply-18-boxes.jpg",
     "name": "Ultra Soft Tissue 2-Ply - 18 Box",
     "rating": {
@@ -191,7 +335,7 @@
     "keywords": ["kleenex", "tissues", "kitchen", "tissues box", "napkins"]
   },
   {
-    "id": "a82c6bac-3067-4e68-a5ba-d827ac0be010",
+    "itemId": "a82c6bac-3067-4e68-a5ba-d827ac0be010",
     "image": "images/products/straw-sunhat.webp",
     "name": "Straw Lifeguard Sun Hat",
     "rating": {
@@ -202,7 +346,7 @@
     "keywords": ["hats", "straw hats", "summer", "apparel"]
   },
   {
-    "id": "e4f64a65-1377-42bc-89a5-e572d19252e2",
+    "itemId": "e4f64a65-1377-42bc-89a5-e572d19252e2",
     "image": "images/products/sky-flower-stud-earrings.webp",
     "name": "Sterling Silver Sky Flower Stud Earrings",
     "rating": {
@@ -213,7 +357,7 @@
     "keywords": ["jewelry", "accessories", "womens"]
   },
   {
-    "id": "b0f17cc5-8b40-4ca5-9142-b61fe3d98c85",
+    "itemId": "b0f17cc5-8b40-4ca5-9142-b61fe3d98c85",
     "image": "images/products/women-stretch-popover-hoodie-black.jpg",
     "name": "Women's Stretch Popover Hoodie",
     "rating": {
@@ -226,7 +370,7 @@
     "sizeChartLink": "images/clothing-size-chart.png"
   },
   {
-    "id": "a93a101d-79ef-4cf3-a6cf-6dbe532a1b4a",
+    "itemId": "a93a101d-79ef-4cf3-a6cf-6dbe532a1b4a",
     "image": "images/products/bathroom-rug.jpg",
     "name": "Bathroom Bath Rug Mat 20 x 31 Inch - Grey",
     "rating": {
@@ -237,7 +381,7 @@
     "keywords": ["bathmat", "bathroom", "home"]
   },
   {
-    "id": "4f4fbcc2-4e72-45cc-935c-9e13d79cc57f",
+    "itemId": "4f4fbcc2-4e72-45cc-935c-9e13d79cc57f",
     "image": "images/products/women-knit-ballet-flat-black.jpg",
     "name": "Women's Knit Ballet Flat",
     "rating": {
@@ -248,7 +392,7 @@
     "keywords": ["shoes", "flats", "womens", "footwear"]
   },
   {
-    "id": "8b5a2ee1-6055-422a-a666-b34ba28b76d4",
+    "itemId": "8b5a2ee1-6055-422a-a666-b34ba28b76d4",
     "image": "images/products/men-golf-polo-t-shirt-blue.jpg",
     "name": "Men's Regular-Fit Quick-Dry Golf Polo Shirt",
     "rating": {
@@ -261,7 +405,7 @@
     "sizeChartLink": "images/clothing-size-chart.png"
   },
   {
-    "id": "b86ddc8b-3501-4b17-9889-a3bad6fb585f",
+    "itemId": "b86ddc8b-3501-4b17-9889-a3bad6fb585f",
     "image": "images/products/trash-can-with-foot-pedal-50-liter.jpg",
     "name": "Trash Can with Foot Pedal - Brushed Stainless Steel",
     "rating": {
@@ -272,7 +416,7 @@
     "keywords": ["garbage", "bins", "cans", "kitchen"]
   },
   {
-    "id": "19c6a64a-5463-4d45-9af8-e41140a4100c",
+    "itemId": "19c6a64a-5463-4d45-9af8-e41140a4100c",
     "image": "images/products/duvet-cover-set-blue-twin.jpg",
     "name": "Duvet Cover Set with Zipper Closure",
     "rating": {
@@ -283,7 +427,7 @@
     "keywords": ["bedroom", "bed sheets", "sheets", "covers", "home"]
   },
   {
-    "id": "d2785924-743d-49b3-8f03-ec258e640503",
+    "itemId": "d2785924-743d-49b3-8f03-ec258e640503",
     "image": "images/products/women-chunky-beanie-gray.webp",
     "name": "Women's Chunky Cable Beanie - Gray",
     "rating": {
@@ -301,7 +445,7 @@
     ]
   },
   {
-    "id": "ee1f7c56-f977-40a4-9642-12ba5072e2b0",
+    "itemId": "ee1f7c56-f977-40a4-9642-12ba5072e2b0",
     "image": "images/products/men-chino-pants-beige.jpg",
     "name": "Men's Classic-fit Pleated Chino Pants",
     "rating": {
@@ -312,7 +456,7 @@
     "keywords": ["pants", "apparel", "mens"]
   },
   {
-    "id": "1c079479-8586-494f-ab53-219325432536",
+    "itemId": "1c079479-8586-494f-ab53-219325432536",
     "image": "images/products/men-athletic-shoes-green.jpg",
     "name": "Men's Athletic Sneaker",
     "rating": {
@@ -323,7 +467,7 @@
     "keywords": ["shoes", "running shoes", "footwear", "mens"]
   },
   {
-    "id": "4df68c27-fd59-4a6a-bbd1-e754ddb6d53c",
+    "itemId": "4df68c27-fd59-4a6a-bbd1-e754ddb6d53c",
     "image": "images/products/men-navigator-sunglasses-brown.jpg",
     "name": "Men's Navigator Sunglasses Pilot",
     "rating": {
@@ -334,7 +478,7 @@
     "keywords": ["sunglasses", "glasses", "accessories", "shades"]
   },
   {
-    "id": "4e37dd03-3b23-4bc6-9ff8-44e112a92c64",
+    "itemId": "4e37dd03-3b23-4bc6-9ff8-44e112a92c64",
     "image": "images/products/non-stick-cooking-set-15-pieces.webp",
     "name": "Non-Stick Cookware Set, Pots, Pans and Utensils - 15 Pieces",
     "rating": {
@@ -345,7 +489,7 @@
     "keywords": ["cooking set", "kitchen"]
   },
   {
-    "id": "a434b69f-1bc1-482d-9ce7-cd7f4a66ce8d",
+    "itemId": "a434b69f-1bc1-482d-9ce7-cd7f4a66ce8d",
     "image": "images/products/vanity-mirror-silver.jpg",
     "name": "Vanity Mirror with Heavy Base - Chrome",
     "rating": {
@@ -356,7 +500,7 @@
     "keywords": ["bathroom", "washroom", "mirrors", "home"]
   },
   {
-    "id": "a45cfa0a-66d6-4dc7-9475-e2b01595f7d7",
+    "itemId": "a45cfa0a-66d6-4dc7-9475-e2b01595f7d7",
     "image": "images/products/women-french-terry-fleece-jogger-camo.jpg",
     "name": "Women's Fleece Jogger Sweatpant",
     "rating": {
@@ -367,7 +511,7 @@
     "keywords": ["pants", "sweatpants", "jogging", "apparel", "womens"]
   },
   {
-    "id": "d339adf3-e004-4c20-a120-40e8874c66cb",
+    "itemId": "d339adf3-e004-4c20-a120-40e8874c66cb",
     "image": "images/products/double-elongated-twist-french-wire-earrings.webp",
     "name": "Double Oval Twist French Wire Earrings - Gold",
     "rating": {
@@ -378,7 +522,7 @@
     "keywords": ["accessories", "womens"]
   },
   {
-    "id": "d37a651a-d501-483b-aae6-a9659b0757a0",
+    "itemId": "d37a651a-d501-483b-aae6-a9659b0757a0",
     "image": "images/products/round-airtight-food-storage-containers.jpg",
     "name": "Round Airtight Food Storage Containers - 5 Piece",
     "rating": {
@@ -389,7 +533,7 @@
     "keywords": ["boxes", "food containers", "kitchen"]
   },
   {
-    "id": "0d7f9afa-2efe-4fd9-b0fd-ba5663e0a524",
+    "itemId": "0d7f9afa-2efe-4fd9-b0fd-ba5663e0a524",
     "image": "images/products/coffeemaker-with-glass-carafe-black.jpg",
     "name": "Coffeemaker with Glass Carafe and Reusable Filter - 25 Oz, Black",
     "rating": {
@@ -400,7 +544,7 @@
     "keywords": ["coffeemakers", "kitchen", "appliances"]
   },
   {
-    "id": "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
+    "itemId": "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
     "image": "images/products/blackout-curtains-black.jpg",
     "name": "Blackout Curtains Set 42 x 84-Inch - Black, 2 Panels",
     "rating": {
@@ -411,7 +555,7 @@
     "keywords": ["bedroom", "home"]
   },
   {
-    "id": "8a53b080-6d40-4a65-ab26-b24ecf700bce",
+    "itemId": "8a53b080-6d40-4a65-ab26-b24ecf700bce",
     "image": "images/products/cotton-bath-towels-teal.webp",
     "name": "100% Cotton Bath Towels - 2 Pack, Light Teal",
     "rating": {
@@ -422,7 +566,7 @@
     "keywords": ["bathroom", "home", "towels"]
   },
   {
-    "id": "10ed8504-57db-433c-b0a3-fc71a35c88a1",
+    "itemId": "10ed8504-57db-433c-b0a3-fc71a35c88a1",
     "image": "images/products/knit-athletic-sneakers-pink.webp",
     "name": "Waterproof Knit Athletic Sneakers - Pink",
     "rating": {
@@ -433,7 +577,7 @@
     "keywords": ["shoes", "running shoes", "footwear", "womens"]
   },
   {
-    "id": "77a845b1-16ed-4eac-bdf9-5b591882113d",
+    "itemId": "77a845b1-16ed-4eac-bdf9-5b591882113d",
     "image": "images/products/countertop-blender-64-oz.jpg",
     "name": "Countertop Blender - 64oz, 1400 Watts",
     "rating": {
@@ -444,7 +588,7 @@
     "keywords": ["food blenders", "kitchen", "appliances"]
   },
   {
-    "id": "36c64692-677f-4f58-b5ec-0dc2cf109e27",
+    "itemId": "36c64692-677f-4f58-b5ec-0dc2cf109e27",
     "image": "images/products/floral-mixing-bowl-set.jpg",
     "name": "10-Piece Mixing Bowl Set with Lids - Floral",
     "rating": {
@@ -455,7 +599,7 @@
     "keywords": ["mixing bowls", "baking", "cookware", "kitchen"]
   },
   {
-    "id": "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
+    "itemId": "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
     "image": "images/products/kitchen-paper-towels-30-pack.jpg",
     "name": "2-Ply Kitchen Paper Towels - 30 Pack",
     "rating": {
@@ -466,7 +610,7 @@
     "keywords": ["kitchen", "kitchen towels", "tissues"]
   },
   {
-    "id": "bc2847e9-5323-403f-b7cf-57fde044a955",
+    "itemId": "bc2847e9-5323-403f-b7cf-57fde044a955",
     "image": "images/products/men-cozy-fleece-zip-up-hoodie-red.jpg",
     "name": "Men's Full-Zip Hooded Fleece Sweatshirt",
     "rating": {
